84472830d6efd309dc008049b31bb01f9f94a1e6
Eben English <eenglish@bpl.org>
2020-02-24 16:58:40 -0500

    DRY out AuthoritiesController specs

--- spec/controllers/bpldc/authorities_controller_spec.rb
+++ spec/controllers/bpldc/authorities_controller_spec.rb
@@ -2,28 +2,10 @@
 
 require_relative './shared/index_shared'
 RSpec.describe Bpldc::AuthoritiesController do
-  describe 'GET index' do
-    before(:each) { get :index }
-    it_behaves_like 'bpldc_index_shared'
-  end
-
-  describe 'GET subjects' do
-    before(:each) { get :subjects }
-    it_behaves_like 'bpldc_index_shared'
-  end
-
-  describe 'GET genres' do
-    before(:each) { get :genres }
-    it_behaves_like 'bpldc_index_shared'
-  end
-
-  describe 'GET names' do
-    before(:each) { get :names }
-    it_behaves_like 'bpldc_index_shared'
-  end
-
-  describe 'GET geographics' do
-    before(:each) { get :geographics }
-    it_behaves_like 'bpldc_index_shared'
+  describe 'GET various authorities routes' do
+    %i[index subjects genres names geographics].each do |authorities_route|
+      before(:each) { get authorities_route }
+      it_behaves_like 'bpldc_index_shared'
+    end
   end
 end
